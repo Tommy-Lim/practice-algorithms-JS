21fc2b6725a7c9a9fd6166d8ef9c4b1ecc4b2244
Tommy-Lim <tlim24@gmail.com>
2016-12-02 14:25:36 -0800

    added isUnique

--- sorted-array-unique-repeated-over-4-times.js
+++ sorted-array-unique-repeated-over-4-times.js
@@ -47,6 +47,33 @@ function max(array){
   }
 }
 
+// Write a function called isUnique that uses a nested
+// for loop to compare each element with every other
+// element. The function should return false if there
+// are any two identical values at different indexes of
+// the array.
+function isUnique(a) {
+  for(i=0; i<a.length; i++){
+    for(j=i+1; j<a.length; j++){
+      if(a[i]==a[j]){
+        console.log("false");
+        return false;
+      }
+    }
+  }
+  console.log("true");
+  return true;
+}
+
+if(isUnique([2,2,3])) {
+  console.log("yup.");
+}
+
+if(isUnique([1,2,3])) {
+  console.log("yup.");
+}
+
+
 console.log(moreThanFour(a1));
 console.log(moreThanFour(a2));
 console.log(moreThanFour(a3));
